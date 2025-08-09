5268878ae664445e3d5f87c5d47ac49773751ac0
viknsagit <vikn.sagit@outlook.com>
2025-08-09 15:13:50 +0500

    Get addons collection by url. Create file if user want.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
+using System.Text;
+using HtmlAgilityPack;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter collection URL");
+
+var collectionUrl = Console.ReadLine();
+var parser = new HtmlWeb();
+var page = parser.Load(collectionUrl);
+var addonsCollection = page.DocumentNode.SelectNodes("//div[@class='collectionItem']");
+
+Console.WriteLine("Addons count: " + addonsCollection.Count);
+Console.WriteLine("Generating workshop.lua file");
+
+var builder = new StringBuilder();
+foreach (var item in addonsCollection)
+{
+    builder.AppendLine($"resource.AddWorkshop(\"{item.Id.Split('_')[1]}\")");
+}
+
+Console.WriteLine(builder.ToString());
+Console.WriteLine("Save to file? (y/n)");
+
+var action = Console.ReadLine();
+if (action is "y" or "yes" or "")
+    CreateWorkshopFile(builder.ToString());
+Process.Start("explorer.exe", Environment.CurrentDirectory);
+return;
+
+
+void CreateWorkshopFile(string text)
+{
+    var path = Environment.CurrentDirectory + @"\workshop.lua";
+    if (File.Exists(path))
+    {
+        Console.WriteLine($"File {path} already exists");
+        return;
+    }
+    File.WriteAllText(path, text);
+    Console.WriteLine($"File {path} Saved");
+}
